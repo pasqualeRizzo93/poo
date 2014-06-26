ffc5f84aa584bbd4fe9c97721ef7b22c69472088
Francesco De Simone <hello@fdesimone.com>
2014-06-26 15:19:45 +0200

    Aggiunto Ignore *.class Formattata classe astratta

--- src/poo/util/ListaAstratta.java
+++ src/poo/util/ListaAstratta.java
@@ -6,110 +6,108 @@ import java.util.ListIterator;
 
 public abstract class ListaAstratta<T> implements Lista<T> {
 
-	@Override
-	public Iterator<T> iterator() {
+    @Override
+    public Iterator<T> iterator() {
 
-		return null;
+	return null;
+    }
+
+    @Override
+    public int size() {
+	int c = 0;
+	for (T e : this)
+	    c++;
+	return c;
+    }// size
+
+    @Override
+    public void clear() {
+	ListIterator<T> lit = this.listIterator();
+	while (lit.hasNext()) {
+	    lit.next();
+	    lit.remove();
 	}
+    }// clear
 
-	@Override
-	public int size() {
-		int c = 0;
-		for (T e : this)
-			c++;
-		return c;
-	}// size
+    @Override
+    public void addFirst(T elem) {
 
-	@Override
-	public void clear() {
-		ListIterator<T> lit = this.listIterator();
-		while (lit.hasNext()) {
-			lit.next();
-			lit.remove();
-		}
-	}// clear
+    }
 
-	@Override
-	public void addFirst(T elem) {
+    @Override
+    public void addLast(T elem) {
 
+    }
+
+    @Override
+    public T getFirts() {
+	ListIterator<T> lit = this.listIterator();
+	if (lit.hasNext())
+	    return lit.next();
+	return null;
+    }// getFirst
+
+    @Override
+    public T getLast() {
+	ListIterator<T> lit = this.listIterator();
+	while (lit.hasNext()) {
+	    lit.next();
 	}
-
-	@Override
-	public void addLast(T elem) {
-
+	if (!lit.hasNext()) {
+	    return lit.previous();
+	} else {
+	    return null;
 	}
+    }// getLast
 
-	@Override
-	public T getFirts() {
-		ListIterator<T> lit = this.listIterator();
-		if (lit.hasNext())
-			return lit.next();
-		return null;
-	}// getFirst
-
-	@Override
-	public T getLast() {
-		ListIterator<T> lit = this.listIterator();
-		while (lit.hasNext()) {
-			lit.next();
-		}
-		if (!lit.hasNext()) {
-			return lit.previous();
-		} else {
-			return null;
-		}
-	}// getLast
-
-	@Override
-	public T removeFirst() {
-		ListIterator<T> lit = this.listIterator();
-		T elem;
-		if (lit.hasNext()) {
-			elem = lit.next();
-			lit.remove();
-			return elem;
-		} else {
-			return null;
-		}
-	}// removeFirst
-
-	@Override
-	public T removeLast() {
-		ListIterator<T> lit = this.listIterator();
-		T elem;
-		while (lit.hasNext()) {
-			lit.next();
-		}
-		if (!lit.hasNext()) {
-			elem = lit.previous();
-			lit.remove();
-			return elem;
-		} else {
-			return null;
-		}
-	}//removeLast
-
-	@Override
-	public boolean isEmpty() {
-		return !listIterator().hasNext();
+    @Override
+    public T removeFirst() {
+	ListIterator<T> lit = this.listIterator();
+	T elem;
+	if (lit.hasNext()) {
+	    elem = lit.next();
+	    lit.remove();
+	    return elem;
+	} else {
+	    return null;
 	}
+    }// removeFirst
 
-	@Override
-	public boolean isFull() {
-		return false;
+    @Override
+    public T removeLast() {
+	ListIterator<T> lit = this.listIterator();
+	T elem;
+	while (lit.hasNext()) {
+	    lit.next();
 	}
-
-	@Override
-	public void sort(Comparator<T> c) {
-      
+	if (!lit.hasNext()) {
+	    elem = lit.previous();
+	    lit.remove();
+	    return elem;
+	} else {
+	    return null;
 	}
+    }// removeLast
 
-	
+    @Override
+    public boolean isEmpty() {
+	return !listIterator().hasNext();
+    }
 
-	@Override
-	public ListIterator<T> listIterator(int start) {
-		
-		return null;
-	}
+    @Override
+    public boolean isFull() {
+	return false;
+    }
+
+    @Override
+    public void sort(Comparator<T> c) {
+
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int start) {
+
+	return null;
+    }
 
 }
