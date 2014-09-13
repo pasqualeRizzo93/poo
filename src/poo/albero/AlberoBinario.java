313e12b51e78923e817f2ce412c2db1fcde327c6
Pasquale Rizzo <pasquale.rizzo93@gmail.com>
2014-09-13 16:07:56 +0200

    modifiche

--- src/poo/albero/AlberoBinario.java
+++ src/poo/albero/AlberoBinario.java
@@ -2,81 +2,50 @@ package poo.albero;
 import java.util.*;
 
 public class AlberoBinario {
-	private static class Nodo {
-		Nodo figlioS, figlioD;
-	}//Nodo
-
-	String parola;
+	private static class Nodo<E> {
+	E info;
+	Nodo<E> figlioS, figlioD;
+	}
 	
-private static class NodoParola extends Nodo{
-	String info;
-	public String toString(){return ""+info;}
-}//NodoParola
+private Nodo<String> radice = null;
 
-private static class NodoOperatore extends Nodo{
-	char can;
-	public String toString(){return ""+can;};
-}//NodoOperatre
-
-private Nodo radice = null;
-
-public void build(String parola) {
-	StringTokenizer st = new StringTokenizer(parola, "", true);
-	if(parola == this.parola)
-	radice = buildFrase(st);
+public void build(String s) {
+	StringTokenizer st = new StringTokenizer(s,"#");
+	String parola;
+	while(st.hasMoreTokens()) {
+		
+		parola = st.nextToken();
+		this.add(radice, parola);
+	}
 }
 
-private Nodo buildFrase (StringTokenizer st){
-	Nodo radice = buildParola(st);
-	while(st.hasMoreTokens()){
-		char can=st.nextToken().charAt(0);
-		if(can == '#') return radice;
-		NodoOperatore operatore = new NodoOperatore();
-		operatore.can = can; operatore.figlioS=radice;
-		Nodo prl = buildParola(st);
-		operatore.figlioD = prl;
-		radice = operatore;
-	}
-	return radice;
-}//buildFrase
-
-public Nodo buildParola(StringTokenizer st){
-	String prl = st.nextToken();
-	if(prl== ""){return buildFrase(st);}
-	else {
-		NodoParola parola = new NodoParola();
-		parola.info = prl;
-		parola.figlioS = null; parola.figlioD = null; return parola;
-	}
-}//buildParola
-
-private void inOrder(Nodo radice){
-	if(radice == null) { System.out.println("Null");
+private void add(Nodo<String> n,String parola) {
+	if(n == null) {
+		n.info = parola;
 	} else {
-	if(radice instanceof NodoOperatore) System.out.print('#');
-	inOrder(radice.figlioS);
-	System.out.print(radice);
-	inOrder(radice);
-	if(radice instanceof NodoOperatore) System.out.print('#');
+		if(n.figlioS  == null) {
+			this.add(n.figlioS, parola);
+		} else if(n.figlioD == null) {
+			this.add(n.figlioD, parola);
+		} else if(n.figlioS != null && n.figlioD!= null) {
+			this.add(n.figlioS, parola);
+		}
 	}
-	}//inOrder
+}
 
-private void preOrder(Nodo radice) {
-	if(radice == null) {
-		System.out.println("Null");  
-	} else {
-		System.out.print(radice+"");
+public String preOrder() {
+	String risultato = preOrder(radice);
+	return risultato;
+}
+
+private String preOrder(Nodo<String> radice){
+	String risultato = "";
+	if(radice!= null){
+		risultato = radice.info+"#";
 		preOrder(radice.figlioS);
 		preOrder(radice.figlioD);
 	}
-}//preOrder
-	
-private void postOrder(Nodo radice){
-	if( radice == null) System.out.println("Null");
-	postOrder(radice.figlioS);
-	postOrder(radice.figlioD);
-	System.out.print(radice+"");
-	}
+	return risultato;
+}
 
-	}
-
+}
